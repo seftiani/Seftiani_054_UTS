9746553cde801a09665740e76312c972ef7a2087
seftiani <82456283+seftiani@users.noreply.github.com>
2022-04-17 14:17:29 +0700

    Add files via upload

---
+++ UTS_Ayu/NO.5/laporan_tabel.php
@@ -0,0 +1,130 @@
+<?php
+
+header("Content-type: application/vnd-ms-excel");
+header("Content-Disposition: attachment; filename=laporan-tabel.xls"); 
+
+?>
+
+<p align="left" style="font-weight:bold;font-size:16pt">Data Mahasiswa Prodi Pendidikan Teknologi Informasi</p>
+
+<table border="1" align="center">
+    <tr>
+        <th bgcolor="#FFFF00" width="50">NO</th>
+        <th bgcolor="#FFFF00" width="100">NIM</th>
+        <th bgcolor="#FFFF00" width="200">NAMA</th>
+        <th bgcolor="#FFFF00" width="50">JK</th>
+        <th bgcolor="#FFFF00" width="200">PRODI</th>
+        <th bgcolor="#FFFF00" width="500">ALAMAT</th>
+        <th bgcolor="#FFFF00" width="200">KOTA</th>
+        <th bgcolor="#FFFF00" width="300">EMAIL</th>
+</tr>
+<tr>
+    <td align="center">1</td>
+    <td align="center">20050974054</td>
+    <td align="center">Seftiani Ayu Lestari</td>
+    <td align="center">P</td>
+    <td align="center">S1 Pendidikan Teknologi Informasi</td>
+    <td align="center">Rt.003/Rw.003, Ds. Nglawak, Kec. Kertosono, Kab. Nganjuk</td>
+    <td align="center">Nganjuk</td>
+    <td align="center">seftianiayu.20054@mhs.unesa.ac.id</td>
+</tr>
+<tr>
+    <td align="center">2</td>
+    <td align="center">20050974002</td>
+    <td align="center">Mela Imroatus Solikhah</td>
+    <td align="center">P</td>
+    <td align="center">S1 Pendidikan Teknologi Informasi</td>
+    <td align="center">Kebonsari Elveka-Graha Mas No.43, Jambangan</td>
+    <td align="center">Surabaya</td>
+    <td align="center">mela.20002@mhs.unesa.ac.id</td>
+</tr>
+<tr>
+    <td align="center">3</td>
+    <td align="center">20050974053</td>
+    <td align="center">Ananda Irvan Tri Kurniawan</td>
+    <td align="center">L</td>
+    <td align="center">S1 Pendidikan Teknologi Informasi</td>
+    <td align="center">Jl. Ketintang Baru</td>
+    <td align="center">Surabaya</td>
+    <td align="center">anandairvan.20053@mhs.unesa.ac.id</td>
+</tr>
+<tr>
+    <td align="center">4</td>
+    <td align="center">20050974057</td>
+    <td align="center">Okky Anugerah Arrohman</td>
+    <td align="center">L</td>
+    <td align="center">S1 Pendidikan Teknologi Informasi</td>
+    <td align="center">Perum. Bumi Persada Hijau (Batara), Kec. Benjeng</td>
+    <td align="center">Gresik</td>
+    <td align="center">okky.20057@mhs.unesa.ac.id</td>
+</tr>
+<tr>
+    <td align="center">5</td>
+    <td align="center">20050974005</td>
+    <td align="center">Shelma Imara Bakir</td>
+    <td align="center">P</td>
+    <td align="center">S1 Pendidikan Teknologi Informasi</td>
+    <td align="center">Tata Residence B7/6 Rt 04 Rw 03. Ds. Banjaran</td>
+    <td align="center">Gresik</td>
+    <td align="center">nimas.4056@mhs.unesa.ac.id</td>
+</tr>
+<tr>
+    <td align="center">6</td>
+    <td align="center">20050974008</td>
+    <td align="center">Shaffa Salsabila</td>
+    <td align="center">P</td>
+    <td align="center">S1 Pendidikan Teknologi Informasi</td>
+    <td align="center">Wonosari Lor Baru 4/15-A Rt 05 Rw 14</td>
+    <td align="center">Surabaya</td>
+    <td align="center">shaffa.20008@mhs.unesa.ac.id</td>
+</tr>
+<tr>
+    <td align="center">7</td>
+    <td align="center">20050974006</td>
+    <td align="center">Nawang Indar Pratiwi</td>
+    <td align="center">P</td>
+    <td align="center">S1 Pendidikan Teknologi Informasi</td>
+    <td align="center">Dsn. Gedangan Rt 01 Rw 01</td>
+    <td align="center">Tulungagung</td>
+    <td align="center">nawangindar.20006@mhs.unesa.ac.id</td>
+</tr>
+<tr>
+    <td align="center">8</td>
+    <td align="center">20050974034</td>
+    <td align="center">Muslich Wahyu Romadhon</td>
+    <td align="center">L</td>
+    <td align="center">S1 Pendidikan Teknologi Informasi</td>
+    <td align="center">Dsn. Galang Rt 02 Rw 02</td>
+    <td align="center">Lamongan</td>
+    <td align="center">muslich.20034@mhs.unesa.ac.id</td>
+</tr>
+<tr>
+    <td align="center">9</td>
+    <td align="center">20050974011</td>
+    <td align="center">Rara Kirana Aisyah Anwar</td>
+    <td align="center">P</td>
+    <td align="center">S1 Pendidikan Teknologi Informasi</td>
+    <td align="center">Dsn. Plumpung Rejo Rt 07 Rw 04, Kec. Pare</td>
+    <td align="center">Kediri</td>
+    <td align="center">rarakirana.20011@mhs.unesa.ac.id</td>
+</tr>
+<tr>
+    <td align="center">10</td>
+    <td align="center">20050974028</td>
+    <td align="center">M. Zainul Ulum</td>
+    <td align="center">L</td>
+    <td align="center">S1 Pendidikan Teknologi Informasi</td>
+    <td align="center">Bandarsari Rt 03 Rw 02</td>
+    <td align="center">Surabaya</td>
+    <td align="center">zainululum.20028@mhs.unesa.ac.id</td>
+</tr>
+</table>
+
+<p align="center">
+<input type="button" value="Export Excel" onclick="window.open('laporan-excel.php')">
+</p>
+
+
+
+
+
